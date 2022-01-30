5a9ee83d593fb63a9f60d0306182e9ff5051cbf8
taehoonhwang <on91716@gmail.com>
2022-01-31 00:51:39 +0900

    second commit

--- afternoon.go
+++ afternoon.go
@@ -3,6 +3,6 @@ package afternoon
 import "fmt"
 
 func GoodAfternoon(name string) string {
-	message := fmt.Sprintf("Hey %v, Good afternoon!", name)
+	message := fmt.Sprintf("[v2] Hey %v, Good afternoon!", name)
 	return message
 }
